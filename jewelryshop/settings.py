ddb09d962967bdee7f83f642abf057a8561ca755
fahadpktn <fahadpktn@gmail.com>
2021-11-08 20:02:09 +0530

    edit

--- jewelryshop/settings.py
+++ jewelryshop/settings.py
@@ -15,9 +15,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3%y3laftm62q0zaj+s7#p-xqq9(&#q+)s8)p-&#&bz*0$!xu$0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS =  ['ecommerce-jewellery.herokuapp.com']
+ALLOWED_HOSTS =  ['ecommerce-jewellery.herokuapp.com', 'localhost', '127.0.0.1:8000']
 
 
 # Application definition
